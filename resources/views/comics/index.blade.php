3e99beba76659cd3831595d84135e1670576c031
salamone-gius <salamone.gius@gmail.com>
2022-07-14 19:46:39 +0200

    minimal style added

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -8,15 +8,20 @@
 
 @section('page-content')
     <h1>All comics</h1>
-    <ul>
-        @foreach ($comics as $comic)
-            <li>
-                <h2>Title: {{$comic->title}}</h2>
-                <p>{{$comic->description}}</p>
-                <h3>Series: {{$comic->series}}</h3>
-                <h3>Type: {{$comic->type}}</h3>
-                <h3>Price: $ {{$comic->price}}</h3>
-            </li>
-        @endforeach
-    </ul>
+    <div class="container">
+        <ul>
+            @foreach ($comics as $comic)
+                <li>
+                    <img src="{{$comic->thumb}}" alt="">
+                    <a href="">
+                        <h2>Title: {{$comic->title}}</h2>
+                    </a>
+                    <p>{{$comic->description}}</p>
+                    <h3>Series: {{$comic->series}}</h3>
+                    <h3>Type: {{$comic->type}}</h3>
+                    <h3>Price: $ {{$comic->price}}</h3>
+                </li>
+            @endforeach
+        </ul>
+    </div>
 @endsection
