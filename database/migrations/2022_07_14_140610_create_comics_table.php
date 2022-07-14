44de26180ca4ef46bc57eca961314d2264a7086a
salamone-gius <salamone.gius@gmail.com>
2022-07-14 16:21:50 +0200

    table created

--- database/migrations/2022_07_14_140610_create_comics_table.php
+++ database/migrations/2022_07_14_140610_create_comics_table.php
@@ -15,6 +15,13 @@ class CreateComicsTable extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 50);
+            $table->text('description');
+            $table->string('thumb', 200)->nullable;
+            $table->float('price', 6, 2);
+            $table->string('series', 50);
+            $table->date('sale_date');
+            $table->string('type', 50);
             $table->timestamps();
         });
     }
