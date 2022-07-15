69aa6acbf50b180086c79542e7ccd668fcfdf612
salamone-gius <salamone.gius@gmail.com>
2022-07-15 17:45:35 +0200

    form in edit.blade modified

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -14,33 +14,36 @@
                 {{-- passo il metodo PUT (per fare un aggiornamento TOTALE (PATCH -> aggiornamento PARZIALE) del comic in tutti i suoi campi) attraverso blade con @method --}}
                 @method('PUT')
 
+                {{-- aggiungo l'attributo value se voglio far comparire all'utente i valori originali prima della modifica--}}
                 <label for="new-title">Title:</label><br>
-                <input type="text" id="new-title" name="title"><br><br>
+                <input type="text" id="new-title" name="title" value="{{$comic->title}}"><br><br>
 
                 <label for="new-series">Series:</label><br>
-                <input type="text" id="new-series" name="series"><br><br>
+                <input type="text" id="new-series" name="series" value="{{$comic->series}}"><br><br>
 
                 <label for="new-thumb">Thumb:</label><br>
-                <input type="text" id="new-thumb" name="thumb"><br><br>
+                <input type="text" id="new-thumb" name="thumb" value="{{$comic->thumb}}"><br><br>
 
+                {{-- nel tag select, avendo già il value pieno, faccio un controllo dentro il tag option--}}
                 <label for="new-type">Choose a type:</label><br>
                 <select name="type" id="new-type">
-                    <option disabled selected></option>
-                    <option value="comic book">Comic book</option>
-                    <option value="graphic novel">Graphic novel</option>
+                    <option disabled {{$comic->type == '' ? 'selected' : ''}}></option>
+                    <option value="comic book" {{$comic->type == 'comic book' ? 'selected' : ''}}>Comic book</option>
+                    <option value="graphic novel" {{$comic->type == 'graphic novel' ? 'selected' : ''}}>Graphic novel</option>
                 </select><br><br>
 
+                {{-- il tag textarea non prevede value, si mette dentro --}}
                 <label for="new-description">Description:</label><br>
-                <textarea id="new-description" name="description"></textarea><br><br>
+                <textarea id="new-description" name="description">{{$comic->description}}</textarea><br><br>
  
                 <label for="new-price">Price:</label><br>
-                <input type="number" id="new-price" required name="price" min="0" value="0" step="any"><br><br>
+                <input type="number" id="new-price" required name="price" min="0" value="{{$comic->price}}" step="any"><br><br>
 
                 <label for="new-sale-date">Sale date:</label><br>
-                <input type="date" id="new-sale-date" name="sale_date"><br><br>
+                <input type="date" id="new-sale-date" name="sale_date" value="{{$comic->sale_date}}"><br><br>
 
                 <div>
-                    <input class="margin-top" type="submit" value="Submit">
+                    <input class="margin-top" type="submit" value="Edit">
                 </div>
             </form>
         </div>
